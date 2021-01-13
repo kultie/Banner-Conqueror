8241aaf7e9b2dadee070a24917106ce2904f6cd2
Nguyễn Thanh Long <longnt@ftech.ai>
2021-01-13 17:33:04 +0700

    Add DataBox

---
+++ Assets/Scripts/Manager/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : ManagerBase<DataManager>
+{
+    [SerializeField]
+    Databox.DataboxObject gameData;
+    private void Awake()
+    {
+        gameData.LoadDatabase();
+    }
+    protected override DataManager GetInstance()
+    {
+        return this;
+    }
+
+}
